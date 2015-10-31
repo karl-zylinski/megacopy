3add2b5b3e460fab5eec8eea787ee49087c3f41d
Karl Zylinski <karl@zylinski.se>
2015-10-31 02:10:06 +0100

    Update MEGACOPY.C

--- MEGACOPY.C
+++ MEGACOPY.C
@@ -1,7 +1,7 @@
 /*
-	This application is for DOS. It copies files to from one place to another and lets
-	you retry / skip / quit if the operation fails. It's very handy for copying a
-	directory tree off a harddrive onto several floppys.
+	This application is for DOS. It should compile with Turbo C 2.01. It copies files
+	from one place to another and lets you retry / skip / quit if the operation fails.
+	It's very handy for copying a directory tree off a harddrive onto several floppys.
 
 	Written by Karl Zylinski
 	karl@zylinski.se
